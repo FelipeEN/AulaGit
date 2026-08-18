dff9f2fb0a3acfc6159c1caaff55419a1d75e084
FelipeEN <donfelipe21@hotmail.com>
2026-08-18 19:27:53 -0300

    adcionei um api para mostrar meus repositorios do meu gitHub.

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,44 @@ btnContador.addEventListener('click', ()=>{
 
 })
 
+const listaRepositorios = document.getElementById('lista-repositorios')
+
+async function buscarRepositorios (){
+    try{
+        const resposta = await fetch('https://api.github.com/users/FelipeEN/repos')
+
+        const repositorios = await resposta.json()
+
+        listaRepositorios.innerHTML=''
+
+        repositorios.forEach(repo =>{
+          const projeto = document.createElement('article')  
+
+          projeto.classList.add('repositorio')
+
+          projeto.innerHTML= `
+                <h3>${repo.name}</h3>
+                <p> descrição: ${repo.description || 'sem descrição' } </p>
+           
+                <p> linguagem : ${repo.language || 'não informado' } </p>
+                <a href= ${repo.html_url} target = "_blank"> 
+                    Ver no GitHub
+                </a>
+            `
+
+            listaRepositorios.appendChild(projeto)
+
+
+        })
+
+    }catch(erro){
+        console.log(erro)
+        listaRepositorios.innerHTML= ` 
+            <p>
+                Não foi possivel carregar os repositorios.
+            </p>
+         `
+
+    }
+}
+ buscarRepositorios ()
